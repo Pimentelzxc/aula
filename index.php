538872092ba00b1f2aafe2a04d3652fa2335cd92
Henrique Pimentel <henriquepimentel1406@gmail.com>
2026-09-14 16:08:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -18,11 +18,13 @@
 
     <?php
 
+    $nome = "Pimentel";
+
     echo "<div class='container'>";
 
-    echo "<h1>Olá, seja bem-vindo!</h1>";
+    echo "<h1>Olá " . $nome . ", seja bem-vindo!</h1>";
 
-    echo "<p>Esta é a aula de Lucas Luz</p>";
+    echo "<p>Esta é a Aula do Professor Lucas Luz</p>";
 
     echo "<p>Hoje estamos aprendendo os primeiros conceitos de PHP.</p>";
 
