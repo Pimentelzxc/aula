ba1331ccd56a0e1365e0eb619ebb095af8481de4
Henrique Pimentel <henriquepimentel1406@gmail.com>
2026-09-14 15:57:50 -0300

    lalala

--- index.php
+++ index.php
@@ -1,18 +1,37 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
-    <title>AULA LUCAS LUZ</title>
-</head>
-<body>
-    <?php
-        echo "<h1>Olá, seja bem-vindo!</h1>";
-        echo "<p>Esta é a aula de Lucas Luz</p>";
-?>
- 
 
-    
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="index.css">
+
+    <title>AULA LUCAS LUZ</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        echo "<div class='container'>";
+
+        echo "<h1>Olá, seja bem-vindo!</h1>";
+
+        echo "<p>Esta é a aula de Lucas Luz</p>";
+
+        echo "<p>Hoje estamos aprendendo os primeiros conceitos de PHP.</p>";
+
+        echo "<button>Começar aula</button>";
+
+        echo "</div>";
+
+    ?>
+
 </body>
+
 </html>
