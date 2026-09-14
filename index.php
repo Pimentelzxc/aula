c41bab55a4bbfeb357e7cb922ab17c7b22cd9b5c
Henrique Pimentel <henriquepimentel1406@gmail.com>
2026-09-14 16:47:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -18,11 +18,11 @@
 
         if ($idade >= 18) {
 
-         "<p class='maior'> Você é maior de idade.</p>";
+         echo "<p class='maior'> Você é maior de idade.</p>";
 
         } else {
 
-            "<p class='menor'>Você é menor de idade.</p>";
+            echo "<p class='menor'>Você é menor de idade.</p>";
 
         }
 
