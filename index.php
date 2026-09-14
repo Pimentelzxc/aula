b173769404955d3c8e705727b158137d6006dcf1
Henrique Pimentel <henriquepimentel1406@gmail.com>
2026-09-14 16:02:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,21 @@
-<!DOCTYPE html>
+<?php
 
-<html lang="pt-br">
+    echo "<div class='container'>";
 
-<head>
+    echo "<h1>Olá, seja bem-vindo!</h1>";
 
-    <meta charset="UTF-8">
+    echo "<p>Esta é a aula de Lucas Luz</p>";
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    echo "<p>Hoje estamos aprendendo os primeiros conceitos de PHP.</p>";
 
-    <link rel="stylesheet" href="index.css">
+    echo "<button id='botao'>Começar aula</button>";
 
-    <title>AULA LUCAS LUZ</title>
+    echo "</div>";
 
-</head>
+?>
 
-<body>
-
-    <?php
-
-        echo "<div class='container'>";
-
-        echo "<h1>Olá, seja bem-vindo!</h1>";
-
-        echo "<p>Esta é a aula de Lucas Luz</p>";
-
-        echo "<p>Hoje estamos aprendendo os primeiros conceitos de PHP.</p>";
-
-        echo "<button>Começar aula</button>";
-
-        echo "</div>";
-
-    ?>
-
-</body>
-
-</html>
+<script>
+    document.getElementById("botao").addEventListener("click", function() {
+        alert("A aula começou!");
+    });
+</script>
