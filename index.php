ba7993b15466263150c7f614116b51dcecdc5add
Henrique Pimentel <henriquepimentel1406@gmail.com>
2026-09-15 15:20:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,6 @@
         $nome = "";
         $idade = ""
         $resultado = "";
-
-
         if ($idade >=18)
             {
                 $resultado = "você é maior de idade";
@@ -34,17 +32,17 @@
            
 
     ?>
-          <div class='container'>
+         <div class='container'>
 
-          <h1>Informações do Aluno</h1>
+         <h1>Informações do Aluno</h1>
 
-          <form method="POST">
+         <form method="POST" action="index.php">
 
-          <input type="text" id="nome" name="nome" required placeholder="Digite seu nome">
+             <input type="text" id="nome" name="nome" required placeholder="Digite seu nome">
 
-          <input type="number" id="idade" name="idade" required placeholder="Digite sua idade">
+             <input type="number" id="idade" name="idade" required placeholder="Digite sua idade">
 
-                     <button type="submit">Verificar</button>
+             <button type="submit">Verificar</button>
 
 
          </form>
